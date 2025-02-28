e0d26c2b076b64983ebb1b652e77565988feb4a2
Sena Ramadhan <ramadhansena254@gmail.com>
2025-02-28 13:13:20 +0700

    ab

--- lib/pages/login.dart
+++ lib/pages/login.dart
@@ -16,20 +16,40 @@ class Login extends StatelessWidget {
         title: const Text('Login Page'),
       ),
       body: SingleChildScrollView(
-        child: Column(
-          children: [
-            SizedBox(height: 50),
-            Center(
-              child: CircleAvatar(
-                radius: 50,
-                backgroundImage: AssetImage('assets/person.avif'),
+        child: Container(
+          height: MediaQuery.of(context).size.height,
+          decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/person.avif'), fit: BoxFit.cover)
+          ),
+          child: Column(
+            children: [
+              SizedBox(height: 50),
+              Center(
+                child: CircleAvatar(
+                  radius: 50,
+                  backgroundImage: AssetImage('assets/person.avif'),
+                ),
               ),
-            ),
-            Text('Walter White', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
-            Text('Maharastra, IN', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),),
-            Text('Mobile | Web | UI-UX', style: TextStyle(fontSize: 16),),
-
-          ],
+              Text('Walter White', style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),),
+              Text('Maharastra, IN', style: TextStyle(fontSize: 12, fontWeight: FontWeight.bold),),
+              Text('Mobile | Web | UI-UX', style: TextStyle(fontSize: 16),),
+              Row(
+                children: [
+                  Expanded(
+      child: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: Row(
+          children: 
+            List.generate(10, (index) => Text("Item ${index + 1}")),
+          
+        ),
+      ),
+                  ),
+                ],
+              )
+          
+            ],
+          ),
         ),
       ),
     );
